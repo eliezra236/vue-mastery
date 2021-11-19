1166ce5453d26cf8777015d3a0ef794d95449e0b
unknown <eliezra236@gmail.com>
2021-11-19 11:08:27 +0200

    finished it

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-const product = 'Socks'
+const app = Vue.createApp({
+  data() {
+    return {
+      product: 'Boots',
+      description: 'Beautiful one of a kind boots'
+    }
+  }
+})
+
+const mountedApp = app.mount('#app');
